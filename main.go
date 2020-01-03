16b65a97b10e9f553a97b79217ce777f10ab63ff
DESKTOP-KCS1KCG\Drako <mazzantia@hotmail.com>
2020-01-03 15:30:04 +0100

    err handling added

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
 
-func main(){
-	sl:=[]string{"ab","bc","a"}
-	s:="abc"
-	mp:=map[string]bool{}
-	Stringinperm(len(sl),sl,s,mp)
+func main() {
+	sl := []string{"aa", "ab", "c"}
+	s := "abc"
+	mp := map[string]bool{}
+	err := Stringinperm(len(sl), sl, s, mp)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	fmt.Println(mp)
 }
 
@@ -18,25 +23,29 @@ func main(){
 // using the recursive version of heap's permutation algo
 // the complexity is N!
 
-func Stringinperm(k int,sl []string, s string, mp map[string]bool){
-	if k==1{
-		mp[strings.Join(sl,"")]=strings.Contains(strings.Join(sl,""),s)
-		}else{
-		Stringinperm(k-1,sl,s,mp)
-		for i:=0;i < k-1; i ++{
-			if k % 2 == 0 {
-				sl[i],sl[k-1]=sl[k-1],sl[i]
-			}else{
-				sl[0],sl[k-1]=sl[k-1],sl[0]
+func Stringinperm(k int, sl []string, s string, mp map[string]bool) error {
+	if sl == nil || len(sl) == 0 {
+		return fmt.Errorf("Source slice is empty")
+	}
+	if k == 1 {
+		mp[strings.Join(sl, "")] = strings.Contains(strings.Join(sl, ""), s)
+	} else {
+		Stringinperm(k-1, sl, s, mp)
+		for i := 0; i < k-1; i++ {
+			if k%2 == 0 {
+				sl[i], sl[k-1] = sl[k-1], sl[i]
+			} else {
+				sl[0], sl[k-1] = sl[k-1], sl[0]
 			}
-			Stringinperm(k-1,sl,s,mp)
+			Stringinperm(k-1, sl, s, mp)
 		}
 	}
+	return nil
 
 }
+
 // suggested optimization
 // we can optimize using moaization tecnique:
 // we generates a map of true permutations (string found included on a set of strings),
 //we encapsulate the search on a hashe-map (closure) of pre allocated strings
 //using this we can save the N! permutations using a costant time access scan of an hash map.
-
--- mergeintervals/main.go
+++ mergeintervals/main.go
@@ -3,53 +3,59 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
 	"sort"
 )
 
-type interval struct{
+type interval struct {
 	start int
-	end int
+	end   int
 }
 type ByStart []interval
 
-func (a ByStart) Len() int {return len(a)}
-func (a ByStart) Swap(i,j int) {a[i],a[j]=a[j],a[i]}
-func (a ByStart) Less(i,j int) bool{return a[i].start < a[j].start}
+func (a ByStart) Len() int           { return len(a) }
+func (a ByStart) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByStart) Less(i, j int) bool { return a[i].start < a[j].start }
 
-
-func main(){
-	is:=[]interval{
-		{1,3},
-		{5,6},
-		{7,8},
+func main() {
+	is := []interval{
+		{1, 3},
+		{5, 6},
+		{7, 8},
 	}
 
-	newintval:=interval{4,6}
-	newis:=insertInterval(is,newintval)
+	newintval := interval{4, 6}
+	newis, err := insertInterval(is, newintval)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	fmt.Printf("%v interval is merged in %v.\n New slice of intervals is %v",newintval,is,newis)
-
+	fmt.Printf("%v interval is merged in %v.\n New slice of intervals is %v", newintval, is, newis)
 
 }
 
 //insert a new interval to a slice re-arranging the start-end based on the max of the end of intervals
 // the complexity is N as we should travers all the N intervals of the source slice
 
-func insertInterval(sofin []interval, newint interval) []interval{
+func insertInterval(sofin []interval, newint interval) ([]interval, error) {
 
-	res:=[]interval{}
+	if sofin == nil || len(sofin) == 0 {
+		err := fmt.Errorf("Source silce is empty")
+		return nil, err
+	}
+	res := []interval{}
 
-	sofin=append(sofin, newint)
+	sofin = append(sofin, newint)
 	sort.Sort(ByStart(sofin))
-	n:=len(sofin)
-	res=append(res,sofin[0])
-	for i:=1;i<n;i++{
-		if sofin[i].start <= res[len(res)-1].end{
-			res[len(res)-1].end= int(math.Max(float64(res[len(res)-1].end), float64(sofin[i].end)))
-		}else {
-			res=append(res,sofin[i])
+	n := len(sofin)
+	res = append(res, sofin[0])
+	for i := 1; i < n; i++ {
+		if sofin[i].start <= res[len(res)-1].end {
+			res[len(res)-1].end = int(math.Max(float64(res[len(res)-1].end), float64(sofin[i].end)))
+		} else {
+			res = append(res, sofin[i])
 		}
 	}
-	return res
+	return res, nil
 }
-
